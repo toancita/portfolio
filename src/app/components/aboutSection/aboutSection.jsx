6acb11d2fddd3ed4969108fc974b972804d328a6
toancita <antomartinamanzanares@gmail.com>
2025-09-16 15:50:03 -0300

    Feat: add about section with styling

---
+++ src/app/components/aboutSection/aboutSection.jsx
@@ -0,0 +1,32 @@
+'use client';
+
+import styles from './about.module.css';
+import Image from 'next/image';
+
+export default function AboutSection() {
+  return (
+      <section className={styles.about}>
+        <div className={styles.leftContent}>
+          <h1 className={styles.title}>About <span className={`textHighlight ${styles.highlight}`}>myself</span> </h1>
+          <p>Hello there! I'm <strong>Antonella Manzanares</strong>, a passionate and skilled
+            <strong> JavaScript developer specializing in building web applications.</strong>
+          <br/> <br/>I love powerlifting, playing videogames and the pure joy of silly cats.</p>
+        </div>
+
+        <div className={styles.rightContent}>
+          <div className={styles.square}>
+            <Image 
+            src='/images/monita.png'
+            alt="Avatar dibujado"
+            width={353}
+            height={469.95}
+            />
+          </div>
+        </div>
+
+        <div className={styles.skills}>
+
+        </div>
+      </section>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Hero from "./components/hero/hero";
+import AboutSection from "./components/aboutSection/aboutSection";
 
 export default function HomePage() {
   return (
     <>
     <Hero />
+    <AboutSection/>
       {/* 
-      <AboutSection />
       <SkillsSection />
       <ProjectsSection />
       <ContactSection /> */}
